72791d3ddce907b7d326e21a80c87e808e0b5555
VelislavaPetkova <Velislava.Petkova@endava.com>
2023-04-24 15:36:48 +0300

    update solution to task 3

--- 3/Rabbit_carrots/Rabbit_carrots/Program.cs
+++ 3/Rabbit_carrots/Rabbit_carrots/Program.cs
@@ -1,198 +1,180 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/*
+ Example tests:
+
+8
+x--x--xx
+xx-x-xx-
+x--x-x-x
+---xxx--
+-x----xx
+-x----x-
+-xx--xx-
+---x----
+
+
+6
+------
+-xx-x-
+--xxx-
+---x-x
+xx--xx
+x---xx
+
+5
+xx---
+-----
+---xx
+---xx
+---xx
+
+4
+x--x
+-x--
+x--x
+--x-
+
+7
+xx-----
+-x---xx
+-xx--x-
+--xx-x-
+---xxx-
+---x---
+---xx--
+ 
+ */
+
 namespace Rabbit_carrots
 {
     internal class Program
     {
-        public enum Direction
-        {
-            up, down, left, right
-        }
 
         public static char carrot = 'x';
         public static char empty = '-';
 
-        public static bool hasNoNeighbours(int x, int y, char[,] matrix, int dim)
+        public static void goLeft(int x, int y, char[,] matrix, int[,] visited, int dim)
         {
-            if ((x == 0 || matrix[x - 1, y] == empty) && (x == dim - 1 || matrix[x + 1, y] == empty)
-                && (y == 0 || matrix[x, y - 1] == empty) && (y == dim - 1 || matrix[x, y + 1] == empty))
+            //We want to go left from [x,y] but if we are in the first column of the
+            //position on the left contains empty space or visited carrot we will stop here, we don't need to go left
+            if (y == 0 || matrix[x, y - 1] == empty || visited[x, y - 1] == 1)
             {
-                Console.WriteLine("has no neighbours: " + x + ';' + y);
-                return true;
+                return;
             }
 
-            return false;
+            //We will mark the left space as visited and we will start searching from there
+            visited[x, y - 1] = 1;
+
+            //Note, we have come here by going left so if we go right we will be going back to visited carrot so we can skip that 
+            goLeft(x, y - 1, matrix, visited, dim);
+            goUp(x, y - 1, matrix, visited, dim);
+            goDown(x, y - 1, matrix, visited, dim);
         }
 
-        public static void print(char[,] matrix, int dimension)
+        public static void goRight(int x, int y, char[,] matrix, int[,] visited, int dim)
+        {
+
+            //If we are in the last column of the garden or the
+            //position on the left contains empty space or visited carrot we will stop here, we don't need to go right
+            if (y == dim - 1 || matrix[x, y + 1] == empty || visited[x, y + 1] == 1)
+            {
+                return;
+            }
+
+            //Same as before, we mark it as visited and go to the other directions
+            visited[x, y + 1] = 1;
+            goRight(x, y + 1, matrix, visited, dim);
+            goUp(x, y + 1, matrix, visited, dim);
+            goDown(x, y + 1, matrix, visited, dim);
+
+        }
+
+        public static void goUp(int x, int y, char[,] matrix, int[,] visited, int dim)
+        {
+            //If we are at the first row we can't go up
+            if (x == 0 || matrix[x - 1, y] == empty || visited[x - 1, y] == 1)
+            {
+                return;
+            }
+
+            visited[x - 1, y] = 1;
+
+            goLeft(x - 1, y, matrix, visited, dim);
+            goRight(x - 1, y, matrix, visited, dim);
+            goUp(x - 1, y, matrix, visited, dim);
+        }
+
+        public static void goDown(int x, int y, char[,] matrix, int[,] visited, int dim)
+        {
+            //If we are at the last row we can't go down
+            if (x == dim - 1 || matrix[x + 1, y] == empty || visited[x + 1, y] == 1)
+            {
+                return;
+            }
+
+            visited[x + 1, y] = 1;
+            goLeft(x + 1, y, matrix, visited, dim);
+            goRight(x + 1, y, matrix, visited, dim);
+            goDown(x + 1, y, matrix, visited, dim);
+        }
+
+
+
+
+        //This function is here for testing purposes only :)
+        public static void print<T>(T[,] matrix, int dimension)
         {
             for (int i = 0; i < dimension; i++)
             {
 
                 for (int k = 0; k < dimension; k++)
                 {
-                    Console.Write(matrix[i, k]);
+                    Console.Write(matrix[i, k] + " ");
                 }
-                Console.WriteLine();
+                Console.WriteLine("");
 
             }
 
+            Console.WriteLine("----------------------");
+
         }
-        public static void goUp(int x, int y, char[,] matrix, int dim)
+        public static void findGroup(int x, int y, char[,] matrix, int[,] visited, int dim)
         {
-            Console.WriteLine("up");
-
-            if ((x == 0 || matrix[x - 1, y] == empty) && (x == dim - 1 || matrix[x + 1, y] == empty))
-            {
-                goLeft(x, y, matrix, dim);
-                goRight(x, y, matrix, dim);
-            }
-            for (int i = x - 1; i >= 0; i--)
-            {
-                if (matrix[i, y] == carrot)
-                {
-                    matrix[i, y] = empty;
-                    if (hasNoNeighbours(i, y, matrix, dim)) return;
-                    goLeft(i, y, matrix, dim);
-                    goRight(i, y, matrix, dim);
-
-
-                }
-                else
-                {
-                    print(matrix, dim);
-                    Console.WriteLine("///////////");
-                    return;
-                }
-            }
+            /*
+            This is the first carrot of our new group so we mark it as visited and we will go and find all
+            connected unvisited carrots going left, right, up and down          
+            */
+            visited[x, y] = 1;
+            goLeft(x, y, matrix, visited, dim);
+            goRight(x, y, matrix, visited, dim);
+            goUp(x, y, matrix, visited, dim);
+            goDown(x, y, matrix, visited, dim);
 
         }
 
-        public static void goDown(int x, int y, char[,] matrix, int dim)
-        {
-            Console.WriteLine("down");
-
-
-            if ((x == 0 || matrix[x - 1, y] == empty) && (x == dim - 1 || matrix[x + 1, y] == empty))
-            {
-                goLeft(x, y, matrix, dim);
-
-                goRight(x, y, matrix, dim);
-            }
-
-            for (int i = x + 1; i < dim; i++)
-            {
-                if (matrix[i, y] == carrot)
-                {
-                    matrix[i, y] = empty;
-                    if (hasNoNeighbours(i, y, matrix, dim)) return;
-                    goLeft(i, y, matrix, dim);
-
-                    goRight(i, y, matrix, dim);
-
-                }
-                else
-                {
-                    print(matrix, dim);
-                    Console.WriteLine("///////////");
-                    return;
-                }
-            }
-
-        }
-
-        public static void goLeft(int x, int y, char[,] matrix, int dim)
-        {
-            Console.WriteLine("left");
-
-            if ((y == 0 || matrix[x, y - 1] == empty) && (y == dim - 1 || matrix[x, y + 1] == empty))
-            {
-                goUp(x, y, matrix, dim);
-                goDown(x, y, matrix, dim);
-            }
-
-            for (int k = y - 1; k >= 0; k--)
-            {
-                if (matrix[x, k] == carrot)
-                {
-                    matrix[x, k] = empty;
-                    if (hasNoNeighbours(x, k, matrix, dim)) return;
-                    goUp(x, y, matrix, dim);
-                    goDown(x, y, matrix, dim);
-
-                }
-                else
-                {
-                    print(matrix, dim);
-                    Console.WriteLine("///////////");
-                    return;
-                }
-            }
-
-
-        }
-
-        public static void goRight(int x, int y, char[,] matrix, int dim)
-        {
-            Console.WriteLine("right");
-
-            if ((y == 0 || matrix[x, y - 1] == empty) && (y == dim - 1 || matrix[x, y + 1] == empty))
-            {
-                goUp(x, y, matrix, dim);
-                goDown(x, y, matrix, dim);
-            }
-
-            for (int k = y + 1; k < dim; k++)
-            {
-                if (matrix[x, k] == carrot)
-                {
-                    matrix[x, k] = empty;
-                    if (hasNoNeighbours(x, k, matrix, dim)) return;
-                    goUp(x, y, matrix, dim);
-                    goDown(x, y, matrix, dim);
-
-                }
-                else
-                {
-                    print(matrix, dim);
-                    Console.WriteLine("///////////");
-                    return;
-                }
-
-            }
-
-        }
-        public static void findGroup(int x, int y, char[,] matrix, int dim)
-        {
-            for (int i = x; i < dim; i++)
-            {
-                for (int k = y; k < dim; k++)
-                {
-                    goLeft(i, k, matrix, dim);
-                    goRight(i, k, matrix, dim);
-
-                }
-            }
-        }
-
-
-        public static int countJumps(char[,] matrix, int dimension)
+        /*
+         This function will iterate through the garden matrix and every time we find a carrot that hasn't been visited(eaten)
+         it means that it is a part of a new group and we will go and eat all the carrots in the new group.
+         */
+        public static int countJumps(char[,] matrix, int[,] visited, int dimension)
         {
             int count = 0;
             for (int i = 0; i < dimension; i++)
             {
                 for (int k = 0; k < dimension; k++)
                 {
-                    if (matrix[i, k] == carrot)
+                    //If we find a carrot that has been visited it means it is part of a group we have already counted
+                    if (matrix[i, k] == carrot && visited[i, k] == 0)
                     {
-                        findGroup(i, k, matrix, dimension);
-                        print(matrix, dimension);
-                        Console.WriteLine(" new group ");
+                        findGroup(i, k, matrix, visited, dimension);
                         count++;
                     }
                 }
@@ -204,8 +186,14 @@ namespace Rabbit_carrots
 
         static void Main(string[] args)
         {
+
+            Console.Write("Hi there! Please type the dimensions of the garden? : ");
             int dimension = int.Parse(Console.ReadLine());
             char[,] matrix = new char[dimension, dimension];
+            int[,] visited = new int[dimension, dimension];
+
+            Console.WriteLine("Great! Now log your garden sceme and note that 'x' is how we mark carrot and '-' is empty space");
+
 
             for (int i = 0; i < dimension; i++)
             {
@@ -214,13 +202,14 @@ namespace Rabbit_carrots
                 for (int k = 0; k < dimension; k++)
                 {
                     matrix[i, k] = row[k];
+                    visited[i, k] = 0;
                 }
             }
 
-            Console.WriteLine(countJumps(matrix, dimension));
+            Console.WriteLine("The rabbit will need " + countJumps(matrix, visited, dimension) + " jumps :)");
 
-            print(matrix, dimension);
         }
     }
 }
+
 
